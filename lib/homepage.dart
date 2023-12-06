1d7103e8c6376f48aa08802cf8664ceb84cb5db9
Satokamone <satomen1500@icloud.com>
2023-12-06 12:14:43 +0900

    ボタンの追加(エラー)

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,23 +4,51 @@ import 'firstpage.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("地域一覧"),
-      ),
-      body: Center(
-        child: TextButton(
-          child: Text("ツアーを選ぶ"),
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            weekButton('道 央'),
+            weekButton('道 南'),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            weekButton('道 北'),
+            weekButton('道 東'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Expanded weekButton(String buttonText) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: ElevatedButton(
           onPressed: () {
-            // （1） 指定した画面に遷移する
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                    // （2） 実際に表示するページ(ウィジェット)を指定する
-                    builder: (context) => FirstPage()));
+                context, MaterialPageRoute(builder: (context) => FirstPage()));
           },
+          child: Text(buttonText),
         ),
       ),
     );
   }
 }
+      //body: Center(
+        //child: TextButton(
+          //child: Text("ツアーを選ぶ"),
+          //onPressed: () {
+            // （1） 指定した画面に遷移する
+            //Navigator.push(
+                //context,
+                //MaterialPageRoute(
+                    // （2） 実際に表示するページ(ウィジェット)を指定する
+                    //builder: (context) => FirstPage()));
+          //},
+        //),
+      //),
