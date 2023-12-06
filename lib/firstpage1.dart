96433b91049af96200f624729673f9f754c2273d
Satokamone <satomen1500@icloud.com>
2023-12-06 12:14:25 +0900

    画像の削除

--- lib/firstpage1.dart
+++ lib/firstpage1.dart
@@ -9,8 +9,6 @@ class FirstPage extends StatelessWidget {
         title: Text("ツアー一覧"),
       ),
       body: Center(
-        child: Image.network(
-            'https://business-textbooks.com/btextbooks/wp-content/uploads/2021/10/b4740446640313ca0cdb1008b99cbb76-19-1024x683.jpg'),
         child: TextButton(
           child: Text("日程を選ぶ"),
           onPressed: () {
