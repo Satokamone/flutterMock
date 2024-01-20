701e054ea21100d22ba2c4a53f636fb9112f201e
Satokamone <satomen1500@icloud.com>
2024-01-20 16:31:57 +0900

    画像の修正

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,31 +9,36 @@ class HomePage extends StatelessWidget {
         title: Text("地域一覧"),
       ),
       body: Center(
-        child: TextButton(
-          child: Text("ツアーを選ぶ"),
-          onPressed: () {
-            // （1） 指定した画面に遷移する
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    // （2） 実際に表示するページ(ウィジェット)を指定する
-                    builder: (context) => FirstPage()));
-            Center(
-              child: Image.asset('images/北海道地域別.jpg'),
-              //child: TextButton(
-              //child: Text("ツアーを選ぶ"),
-              //onPressed: () {
-              // （1） 指定した画面に遷移する
-              //Navigator.push(
-              //context,
-              //MaterialPageRoute(
-              // （2） 実際に表示するページ(ウィジェット)を指定する
-              //builder: (context) => FirstPage()));
-            );
-          }, //},
+        child: Column(
+          children: [
+            Image.asset('images/北海道地域別.jpg'),
+            TextButton(
+              child: Text("ツアーを選ぶ"),
+              onPressed: () {
+                // （1） 指定した画面に遷移する
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      // （2） 実際に表示するページ(ウィジェット)を指定する
+                      builder: (context) => FirstPage()),
+                );
+                //Center(
+                //child: Image.asset('images/北海道地域別.jpg'),
+                //child: TextButton(
+                //child: Text("ツアーを選ぶ"),
+                //onPressed: () {
+                // （1） 指定した画面に遷移する
+                //Navigator.push(
+                //context,
+                //MaterialPageRoute(
+                // （2） 実際に表示するページ(ウィジェット)を指定する
+                //builder: (context) => FirstPage()));
+              }, //},
+            ),
+          ],
+          //),
         ),
       ),
-      //),
     );
   }
 }
