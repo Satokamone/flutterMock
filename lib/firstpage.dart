00da7d7ad4e2721b6fb7f75df7e79026809532a7
Satokamone <satomen1500@icloud.com>
2024-01-23 17:07:12 +0900

    画像の追加（エラー）

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -5,22 +5,29 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ツアー一覧"),
-      ),
-      body: Center(
-        child: TextButton(
-          child: Text("日程を選ぶ"),
-          onPressed: () {
-            // （1） 指定した画面に遷移する
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    // （2） 実際に表示するページ(ウィジェット)を指定する
-                    builder: (context) => Secondpage()));
-          },
+        appBar: AppBar(
+          title: Text("ツアー一覧"),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Image.asset('images/dounann.jpg'),
+              Image.asset('images/douou.jpg'),
+              Image.asset('images/douhoku.jpg'),
+              Image.asset('images/doutou.jpg'),
+            ],
+          ),
+          child: TextButton(
+            child: Text("日程を選ぶ"),
+            onPressed: () {
+              // （1） 指定した画面に遷移する
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      // （2） 実際に表示するページ(ウィジェット)を指定する
+                      builder: (context) => Secondpage()));
+            },
+          ),
+        ));
   }
 }
