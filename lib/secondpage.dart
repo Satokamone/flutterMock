19873841cdf1db4cc4f004eade39df3a6ddf9acb
Satokamone <satomen1500@icloud.com>
2024-01-23 21:57:19 +0900

    カレンダーの追加（エラー）

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,23 +1,60 @@
 import 'package:flutter/material.dart';
 import 'thirdpage.dart';
 
-class Secondpage extends StatelessWidget {
+class Secondpage extends StatefulWidget {
+  const Secondpage({Key? key}) : super(key: key);
+
+  @override
+  State<Secondpage> createState() => _SecondpageState();
+}
+
+class _SecondpageState extends State<Secondpage> {
+  DateTime selectedDate = DateTime.now();
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("日程選択"),
+        title: const Text("日程選択"),
       ),
       body: Center(
-        child: TextButton(
-          child: Text("決定"),
-          onPressed: () {
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('選択した日付: ${selectedDate.year}/${selectedDate.month}/${selectedDate.day}'),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: const Text('日付選択'),
+              ),
+          ],
+        ),
+        ),
+    )
+
+        Future<void> _selectDate(BuildContext context) async {
+          final DateTime? picked = await showDatePicker(
+            context: context,
+            initialDate: selectedDate,
+            firstDate: DateTime(2024),
+            lastDate: DateTime(2026),
+             );
+
+             if(picked != null) {
+              setState(() {
+                selectedDate = picked;
+              });
+             }
+        }
+      //body: Center(
+        //child: TextButton(
+          //child: Text("決定"),
+          //onPressed: () {
             // （1） 指定した画面に遷移する
-            Navigator.push(
-                context,
-                MaterialPageRoute(
+            //Navigator.push(
+                //context,
+                //MaterialPageRoute(
                     // （2） 実際に表示するページ(ウィジェット)を指定する
-                    builder: (context) => thirdpage()));
+                    //builder: (context) => thirdpage()));
           },
         ),
       ),
